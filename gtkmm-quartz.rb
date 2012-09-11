76b7191a7c783bb30fe1262ede853f30cfff017d
Alex Cornejo <acornejo@gmail.com>
2012-09-11 12:15:09 -0400

    Imported Sharpie's quartz efforts.

---
+++ gtkmm-quartz.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class GtkmmQuartz < Formula
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gtkmm/2.18/gtkmm-2.18.2.tar.gz'
+  homepage 'http://www.gtkmm.org/'
+  md5 '746a901f5b15605c51d8a396415c8504'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'glibmm'
+  depends_on 'libsigc++'
+
+  depends_on 'acornejo/gtk+-quartz' => :alt
+  depends_on 'acornejo/pangomm-quartz' => :alt
+
+  keg_only 'This formula builds GtkMM for use with Quartz instead of X11, which is experimental.'
+
+  def install
+    system './configure', '--disable-dependency-tracking', "--prefix=#{prefix}"
+    system 'make install'
+  end
+end
