d15250ec47b4c42295d1eabf9f61d50a3d946e42
Alex Cornejo <acornejo@gmail.com>
2014-03-17 13:03:56 -0400

    update poppler-quartz

--- poppler-quartz.rb
+++ poppler-quartz.rb
@@ -1,42 +1,23 @@
 require 'formula'
 
-def glib?; ARGV.include? '--with-glib'; end
-def qt?; ARGV.include? '--with-qt4'; end
-
 class PopplerData < Formula
   url 'http://poppler.freedesktop.org/poppler-data-0.4.5.tar.gz'
   md5 '448dd7c5077570e340340706cef931aa'
 end
 
 class PopplerQuartz < Formula
-  url 'http://poppler.freedesktop.org/poppler-0.16.7.tar.gz'
   homepage 'http://poppler.freedesktop.org/'
-  md5 '3afa28e3c8c4f06b0fbca3c91e06394e'
+  url 'http://poppler.freedesktop.org/poppler-0.24.5.tar.xz'
+  sha1 '7b7cabee85bd81a7e55c939740d5d7ccd7c0dda5'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
-  depends_on 'qt' if qt?
-
   depends_on 'acornejo/quartz/cairo-quartz'
 
   keg_only 'This formula builds Poppler for use with Quartz instead of X11, which is experimental.'
 
-  def options
-    [
-      ["--with-qt4", "Build Qt backend"]
-    ]
-  end
-
   def install
-    if qt?
-      ENV['POPPLER_QT4_CFLAGS'] = `#{HOMEBREW_PREFIX}/bin/pkg-config QtCore QtGui --libs`.chomp
-      ENV.append 'LDFLAGS', "-Wl,-F#{HOMEBREW_PREFIX}/lib"
-    end
-
-    args = ["--disable-dependency-tracking", "--prefix=#{prefix}", "--enable-xpdf-headers", '--enable-poppler-glib']
-    # Explicitly disable Qt if not requested because `POPPLER_QT4_CFLAGS` won't
-    # be set and the build will fail.
-    args << ( qt? ? '--enable-poppler-qt4' : '--disable-poppler-qt4' )
+    args = ["--disable-dependency-tracking", "--prefix=#{prefix}", "--enable-xpdf-headers", '--enable-poppler-glib', '--disable-poppler-qt4']
 
     system "./configure", *args
     system "make install"
